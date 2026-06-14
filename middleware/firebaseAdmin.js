1585896f125f0ee5ffd351859bdbb31845365728
Kingvics <okoromivictorsunday@gmail.com>
2026-06-14 09:09:01 +0100

    updated

--- middleware/firebaseAdmin.js
+++ middleware/firebaseAdmin.js
@@ -1,8 +1,8 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-adminsdk-fbsvc-7d3ce0a628.json');
+// const serviceAccount = require('../firebase-adminsdk-fbsvc-7d3ce0a628.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 module.exports = admin;
--- middleware/sendNotification.js
+++ middleware/sendNotification.js
@@ -1,11 +1,11 @@
 const NotificationTokenSchema = require('../model/notificationToken');
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-adminsdk-fbsvc-7d3ce0a628.json');
+// const serviceAccount = require('../firebase-adminsdk-fbsvc-7d3ce0a628.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 const SendNotification = async ({ searchObject, title, body }) => {
   try {
